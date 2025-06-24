7c3d4fb609cb1f713b5cc1769d6610e52fadcc57
ShaodongWei <43374853+ShaodongWei@users.noreply.github.com>
2025-06-24 15:19:10 +0200

    Update variants_finder.py

--- scripts/variants_finder.py
+++ scripts/variants_finder.py
@@ -9,10 +9,14 @@ def mapping():
     parser.add_argument('--index_column', required=True, help='The index column in peptide_table. It should be peptide amino acids.')
     parser.add_argument('--mapping_file', required=True, help="Path to mapping file (csv).")
     parser.add_argument('--output_dir', required=True, help="Output directory where final mapped variants will be located.")
+    parser.add_argument('--variant_min_level', required=True, default=0, type=float, help="The minimal abundance level for variants considered to be present")
     args = parser.parse_args()
     
     # read in mapping file 
     mapping = pd.read_csv(args.mapping_file)
+
+    os.listdir(os.getcwd())
+    mapping = pd.read_csv('./output/mapping.csv')
 
     # read in peptide table 
     dat = pd.read_csv(args.peptide_table, index_col=args.index_column)
@@ -23,7 +27,11 @@ def mapping():
         result = []
         for sample_name in df.columns:
             sample = df[sample_name].dropna()
-            sample_pep_pool = list(set(sample.index[sample != 0]))
+            if args.variant_min_level == 0:
+                sample_pep_pool = list(set(sample.index[sample > 0]))
+            else:
+                sample_pep_pool = list(set(sample.index[sample >= args.variant_min_level]))
+            sample_pep_pool = list(set(sample.index[sample > args.variant_min_level]))
             tmp = mapping[mapping['uniq_human'].isin(sample_pep_pool)].copy()
             tmp['Sample'] = sample_name
             tmp.columns = ['Hb Name', 'Peptides', 'Sample']
